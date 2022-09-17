27a408c587d24ff928358f117c03f5d715941dfa
Julien Savary <63762307+Julien101101@users.noreply.github.com>
2022-09-17 15:02:34 -0700

    'fade_transitions_pt1'

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -13,6 +13,7 @@ import 'package:vs/widgets/color_on_select.dart';
 import 'package:vs/widgets/lines.dart';
 import 'package:vs/widgets/polygon.dart';
 import 'package:vs/widgets/shapes.dart';
+import 'package:animations/animations.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -55,133 +56,145 @@ class _MyHomePageState extends State<MyHomePage> {
         body: Consumer<ShapeViewModel>(builder: (context, shape, child) {
           shape.habits.sort((a, b) => a.index!.compareTo(b.index!));
           return GestureDetector(
-              onVerticalDragUpdate: (details) {
-                print('drag');
-              },
               child: SingleChildScrollView(
-                child: Column(children: [
-                  Container(
-                    padding: shape.habits.isEmpty
-                        ? EdgeInsets.only(top: size.height * 0.3)
-                        : EdgeInsets.only(top: size.height * 0.1),
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        pushNewScreen(context, screen: CreateScreen());
+            child: Column(children: [
+              Container(
+                padding: shape.habits.isEmpty
+                    ? EdgeInsets.only(top: size.height * 0.3)
+                    : EdgeInsets.only(top: size.height * 0.1),
+                child: FloatingActionButton(
+                  onPressed: () {
+                    pushNewScreen(context, screen: CreateScreen());
+                  },
+                  child: Icon(Icons.add),
+                ),
+              ),
+              shape.habits.isEmpty
+                  ? Container(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 20.0),
+                          child: Text('Create a new habit',
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey)),
+                        ),
+                      ))
+                  : ReorderableListView.builder(
+                      itemCount: shape.habits.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return OpenContainer(
+                          transitionType: ContainerTransitionType.fadeThrough,
+                          closedBuilder:
+                              (BuildContext _, VoidCallback openContainer) {
+                            return ListTile(
+                              key: Key(shape.habits[index].id!),
+                              title: Text(shape.habits[index].objectName),
+                              trailing: Icon(Icons.drag_handle),
+                              onTap: () {
+                                openContainer();
+                              },
+                            );
+                          },
+                          openBuilder:
+                              (BuildContext _, VoidCallback openContainer) {return ShapeScreen(shape.habits[index]);},
+                          onClosed: (_) => print('Closed'),
+                        );
                       },
-                      child: Icon(Icons.add),
-                    ),
-                  ),
-                  shape.habits.isEmpty
-                      ? Container(
-                          alignment: Alignment.center,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 20.0),
-                              child: Text('Create a new habit',
-                                  style: TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey)),
-                            ),
-                          ))
-                      : ReorderableListView(
-                          physics: NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          proxyDecorator: proxyDecorator,
-                          children: <Widget>[
-                            for (int index = 0;
-                                index < shape.habits.length;
-                                index += 1)
-                              InkWell(
-                                key: Key('$index'),
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => ShapeScreen(
-                                                habit: shape.habits[index],
-                                              )));
-                                },
-                                child: Container(
-                                    height: size.height * 0.2,
-                                    margin: const EdgeInsets.symmetric(
-                                      vertical: 16.0,
-                                      horizontal: 24.0,
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      proxyDecorator: proxyDecorator,
+                      children: <Widget>[
+                        for (int index = 0;
+                            index < shape.habits.length;
+                            index += 1)
+                          InkWell(
+                            key: Key('$index'),
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ShapeScreen(
+                                            habit: shape.habits[index],
+                                          )));
+                            },
+                            child: Container(
+                                height: size.height * 0.2,
+                                margin: const EdgeInsets.symmetric(
+                                  vertical: 16.0,
+                                  horizontal: 24.0,
+                                ),
+                                child: Stack(
+                                  children: <Widget>[
+                                    Container(
+                                      height: size.height * 0.3,
+                                      margin: EdgeInsets.only(left: 0.0),
+                                      decoration: BoxDecoration(
+                                        color:
+                                            Color.fromARGB(255, 255, 255, 255),
+                                        shape: BoxShape.rectangle,
+                                        borderRadius:
+                                            BorderRadius.circular(8.0),
+                                        boxShadow: <BoxShadow>[
+                                          BoxShadow(
+                                            color: Colors.black12,
+                                            blurRadius: 10.0,
+                                            offset: Offset(0.0, 10.0),
+                                          ),
+                                        ],
+                                      ),
                                     ),
-                                    child: Stack(
-                                      children: <Widget>[
-                                        Container(
-                                          height: size.height * 0.3,
-                                          margin: EdgeInsets.only(left: 0.0),
-                                          decoration: BoxDecoration(
-                                            color: Color.fromARGB(
-                                                255, 255, 255, 255),
-                                            shape: BoxShape.rectangle,
-                                            borderRadius:
-                                                BorderRadius.circular(8.0),
-                                            boxShadow: <BoxShadow>[
-                                              BoxShadow(
-                                                color: Colors.black12,
-                                                blurRadius: 10.0,
-                                                offset: Offset(0.0, 10.0),
-                                              ),
-                                            ],
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      children: [
+                                        Flexible(
+                                          flex: 1,
+                                          child: Container(
+                                              margin: EdgeInsets.symmetric(
+                                                  vertical: 16.0),
+                                              alignment: Alignment.centerRight,
+                                              child: Polygon(
+                                                sides:
+                                                    shape.habits[index].count,
+                                                color: Color(
+                                                    shape.habits[index].color),
+                                              )),
+                                        ),
+                                        Flexible(
+                                          flex: 1,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Text(
+                                                shape.habits[index].objectName,
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.black)),
                                           ),
                                         ),
-                                        Row(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.center,
-                                          children: [
-                                            Flexible(
-                                              flex: 1,
-                                              child: Container(
-                                                  margin: EdgeInsets.symmetric(
-                                                      vertical: 16.0),
-                                                  alignment:
-                                                      Alignment.centerRight,
-                                                  child: Polygon(
-                                                    sides: shape
-                                                        .habits[index].count,
-                                                    color: Color(shape
-                                                        .habits[index].color),
-                                                  )),
-                                            ),
-                                            Flexible(
-                                              flex: 1,
-                                              child: Padding(
-                                                padding:
-                                                    const EdgeInsets.all(8.0),
-                                                child: Text(
-                                                    shape.habits[index]
-                                                        .objectName,
-                                                    style: TextStyle(
-                                                        fontSize: 20,
-                                                        fontWeight:
-                                                            FontWeight.bold,
-                                                        color: Colors.black)),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
                                       ],
-                                    )),
-                              )
-                          ],
-                          onReorder: (int oldIndex, int newIndex) {
-                            setState(() {
-                              if (oldIndex < newIndex) {
-                                newIndex -= 1;
-                              }
-                              final item = shape.habits.removeAt(oldIndex);
-                              shape.editIndex(
-                                  shape.habits[newIndex].id!, newIndex);
-                              shape.habits.insert(newIndex, item);
-                            });
-                          },
-                        )
-                ]),
-              ));
+                                    ),
+                                  ],
+                                )),
+                          )
+                      ],
+                      onReorder: (int oldIndex, int newIndex) {
+                        setState(() {
+                          if (oldIndex < newIndex) {
+                            newIndex -= 1;
+                          }
+                          final item = shape.habits.removeAt(oldIndex);
+                          shape.editIndex(shape.habits[newIndex].id!, newIndex);
+                          shape.habits.insert(newIndex, item);
+                        });
+                      },
+                    )
+            ]),
+          ));
         }));
   }
 
