883b36d13db0701b9fb47c0680ca2359535043eb
julien <juliensavary2001@gmail.com>
2023-12-21 15:58:47 -0800

    comment 8

--- lib/view model/shape_view_model.dart
+++ lib/view model/shape_view_model.dart
@@ -76,7 +76,7 @@ class ShapeViewModel extends BaseViewModel {
         'id': value.id,
       }, SetOptions(merge: true));
     });
-  }  // comment 7
+  }  // comment 8
 
   List<Habits> habits = [];
   getHabit() async {
